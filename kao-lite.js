a1455604d2ea98e46880e4d9cb1a1c708faa3ccf
creep <creep.wei@gmail.com>
2015-07-13 15:41:35 +0800

    add kao.ready

--- kao-lite.js
+++ kao-lite.js
@@ -317,3 +317,91 @@
     }
 
 })(this, document);
+
+(function (win, doc, kao) {
+
+    var domready = false;
+    var readylist = [];
+
+    /*!
+     * contentloaded.js
+     *
+     * Author: Diego Perini (diego.perini at gmail.com)
+     * Summary: cross-browser wrapper for DOMContentLoaded
+     * Updated: 20101020
+     * License: MIT
+     * Version: 1.2
+     *
+     * URL:
+     * http://javascript.nwbox.com/ContentLoaded/
+     * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+     *
+     */
+
+    // @win window reference
+    // @fn function reference
+    function contentLoaded(win, fn) {
+
+        var done = false, top = true,
+
+        doc = win.document, root = doc.documentElement,
+
+        add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+        rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+        pre = doc.addEventListener ? '' : 'on',
+
+        init = function(e) {
+            if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+            (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+            if (!done && (done = true)) fn.call(win, e.type || e);
+        },
+
+        poll = function() {
+            try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+            init('poll');
+        };
+
+        if (doc.readyState == 'complete') fn.call(win, 'lazy');
+        else {
+            if (doc.createEventObject && root.doScroll) {
+                try { top = !win.frameElement; } catch(e) { }
+                if (top) poll();
+            }
+            doc[add](pre + 'DOMContentLoaded', init, false);
+            doc[add](pre + 'readystatechange', init, false);
+            win[add](pre + 'load', init, false);
+        }
+
+    }
+
+    var fireReadyList = function () {
+        var i = 0, fn;
+
+        while ( ( fn = readylist[ i++ ] ) ) {
+            fn.call( this );
+        }
+    };
+
+    contentLoaded( this, function () {
+        domready = true;
+        fireReadyList();
+    } );
+
+    kao.ready = function () {
+        var args = [].slice.call( arguments );
+        var i = 0, fn;
+
+        if ( domready ) {
+            while ( ( fn = args[ i++ ] ) ) {
+                fn.call(win);
+            }
+        } else {
+            while ( fn = args.shift() ) {
+                if ( typeof fn == 'function' ) {
+                    readylist.push( fn );
+                }
+            }
+        }
+    };
+
+})(this, document, kao);
